6d1abc20ed05e765ea509ece3fac343d9746297e
Tobias Fiebig <tobias+git@fiebig.nl>
2024-07-03 16:48:56 +0200

    updated vyos files for munin

--- src/conf_mode/munin-node.py
+++ src/conf_mode/munin-node.py
@@ -30,8 +30,8 @@ from vyos.configdict import dict_merge
 from vyos.configverify import verify_vrf
 from vyos.template import render
 from vyos.template import is_ipv4
-from vyos.util import call
-from vyos.util import chmod_755
+from vyos.utils import call
+from vyos.utils import chmod_755
 from vyos.validate import is_addr_assigned
 from vyos.xml import defaults
 from vyos import ConfigError
